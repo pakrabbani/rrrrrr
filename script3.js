c5254d32c19003fdde778b633758483d83caf7f7
pakrabbani <41202030+pakrabbani@users.noreply.github.com>
2018-07-14 17:30:16 +0500

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,15 @@
+function trafficMassege(color){
+	if(color==="red"){
+ console.log("vehical must stop");
+} else if(color==="yellow"){
+	console.log("vehical should get ready to move")
+
+}
+	else if(color==="green"){
+		console.log("go")
+	}
+	
+	}
+  
+
+trafficMassege("RED");
